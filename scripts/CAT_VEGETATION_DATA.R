2893ea26f28621b756b9751821f5e53f2be7bba5
AlbertSaneCodony <albertsanecodony@gmail.com>
2024-05-05 14:06:05 +0200

    Change the names to match the SpParamsFR

--- scripts/CAT_VEGETATION_DATA.R
+++ scripts/CAT_VEGETATION_DATA.R
@@ -2,28 +2,32 @@ library(readxl)
 library(medfate)
 library(tidyverse)
 
-data(SpParamsES)
+data(SpParamsFR)
 
 #CAT VEGETATION DATA####
 CAT_VEG_DATA <- read_excel("raw_data/CAT_DATA/SPIF_plotvegdata.xlsx", sheet = "vegetation_data")
 
-#CHECK IF THE SP NAME ARE IN SpParamsES
+#CHECK IF THE SP NAME ARE IN SpParamsFR
 
-CAT_VEG_DATA$IS_CORRECT_ES<-CAT_VEG_DATA$Species %in% SpParamsES$Name
+CAT_VEG_DATA$IS_CORRECT_ES<-CAT_VEG_DATA$Species %in% SpParamsFR$Name
 
 #CREATE A WRONG SP DATAFRAME
 
 WRONG_CAT_VEG_DATA<-CAT_VEG_DATA %>% filter(IS_CORRECT_ES==FALSE)
 count(WRONG_CAT_VEG_DATA,Species)
 
-#REPLACE THE OLD NAMES WITH THE CORRECT NAMES FROM SpParamsES
+#REPLACE THE OLD NAMES WITH THE CORRECT NAMES FROM SpParamsFR
 
 old_names <- pull(distinct(WRONG_CAT_VEG_DATA,Species))
-new_names <- c("Rosmarinus officinalis","Thymelaea spp.",
-               "Helianthemum spp.","Thymus spp.","Coronilla spp.",
-               "Euphorbia spp.","Lavandula spp.","Rubus spp.",
-               "Asparagus spp.","Helianthemum spp.","Asphodelus spp.")
-  
+new_names <- c("Rosmarinus officinalis", 
+               "Lotus dorycnium", 
+               "Thymelaea", 
+               "Juniperus oxycedrus subsp. oxycedrus", 
+               "Calicotome spinosa", 
+               "Rubus", 
+               "Asparagus officinalis subsp. officinalis", 
+               "Cistus clusii", 
+               "Asphodelus")
 
 index<-NA
 CAT_VEG_DATA$sp_correct_name<-NA
@@ -49,4 +53,6 @@ write.csv(CAT_VEG_DATA, "data/CAT_VEG_DATA.csv", row.names=FALSE)
 
 #Asphodelus spp. not correct
 
-which(!CAT_VEG_DATA$sp_correct_name %in% SpParamsES$Name)
+which(!CAT_VEG_DATA$sp_correct_name %in% SpParamsFR$Name)
+
+CAT_VEG_DATA[which(!CAT_VEG_DATA$sp_correct_name %in% SpParamsFR$Name),]
